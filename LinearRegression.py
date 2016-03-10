2759431fe9b25bcbc0c39de63e7ea20526ac5cb2
AgnusDei <lucius.cao@gmail.com>
2016-03-10 21:09:37 +0800

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,15 +1,6 @@
 import numpy as np
 
 class LinearRegression:
-	def __init__(self):
-		self.learning_rate = 0.01
-		self.reg_rate = 0.003
-		self.max_iter = 500
-
-	def data_loader():
-		'''including init X and y and theta'''
-		pass
-
 	def create_data(self,number_of_examples, number_of_features):
 		X = np.random.randint(0,100,(number_of_examples,number_of_features))
 		y = np.random.randint(-100,1000,(number_of_examples,1))
@@ -30,12 +21,12 @@ class LinearRegression:
 		new_X = np.column_stack((bias_feature,X))
 		return new_X
 
-	def cost_function(self,X, y, theta):
+	def cost_function(self,X, y, theta, reg_rate = 0):
 		m = len(y)
-		cost = float((1/(2*m))*sum((X.dot(theta)-y).A**2))
+		cost = float((1/(2*m))*sum((X.dot(theta)-y).A**2)) + float(reg_rate * sum((theta[1:,]).A ** 2))
 		return cost
 
-	def gradient_descent(self,X, y, theta):
+	def gradient_descent(self,X, y, theta, reg_rate = 0):
 		m = len(y)
 		grad = np.matrix((1/m)*sum((X.dot(theta)-y).A * X.A))
 		grad = grad.T
@@ -43,11 +34,17 @@ class LinearRegression:
 
 if __name__ == '__main__':
 	lr = LinearRegression()
-	X, y, theta = lr.create_data(500,10)
+	X, y, theta = lr.create_data(10000,50)
 	X = lr.feature_normalize(X)
 	X = lr.add_bias(X)
 	cost = []
-	for i in range(lr.max_iter):
-		grad = lr.gradient_descent(X,y,theta)
-		theta = theta - lr.learning_rate * grad
+	for i in range(500):
+		grad = lr.gradient_descent(X,y,theta,1)
+		theta = theta - 0.003 * grad
 		cost.append(lr.cost_function(X,y,theta))
+	print(cost[-1])
+
+
+
+
+
